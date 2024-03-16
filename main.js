bd845c3ff6b8f7b09a3dad7080bfa693cd0b281d
Your Name <you@example.com>
2024-03-17 00:36:28 +0500

    js

---
+++ main.js
@@ -0,0 +1,94 @@
+// 1-masala
+// let num = +prompt("sonni kiriting")
+// let num_even = 0
+// let num_odd = 0
+// for(let i = 0; i < num; i++) {
+// 	if (i % 2 == 0) {
+// 		num_even += i
+// 	}else{
+// 		num_odd += i
+// 	}
+// }
+// let result = Math.abs(num_even - num_odd)
+// console.log(result)
+
+// 2-masala
+// let num1 = +prompt("num1")
+// let num2 = +prompt("num2")
+// if (num1 < num2) {
+// 	for(i = num1; i <= num2; i++){
+// 		console.log(i)
+// 	}
+// }else{
+// 	for(i = num2; i <= num1; i++){
+// 		console.log(i)
+// 	}
+// }
+
+//3-masala
+// let num = +prompt("sonni kiriting")
+// let num_even = 0
+// for(i = 1; i <= num; i++) {
+// 	if (i % 2 ==0) {
+// 		num_even += 1
+// 	}
+// }
+// console.log(num_even);
+
+//4-masala
+// let str = prompt("so'zni kiriting...")
+// let text = 0
+// for(i = 0; i < str.length; i++){
+// 	text++
+// }
+// console.log(text);
+
+// 5-masala
+// let num = +prompt("sonni kiriting...")
+// let count = 0
+// for(i = 1; i <= num; i++) {
+// 	if (num % i == 0) {
+// 		count++
+// 	}
+// }
+// if (count == 2) {
+// 	console.log('kiritilgan son tub');
+// }else{
+// 	console.log('kiritilgan son tub emas');
+// }
+
+//6-masala
+// let str = prompt("so'zni kiriting...")
+// let count = 0
+// for(let i = 0; i < str.length; i++){
+// 	let l = str[i]
+// 	if (l === "a" || l === "o" || l === "u" || l === "i" || l === "o'" || l === "e") {
+// 		count++
+// 	}
+// }
+// console.log(count);
+
+//7-masala
+// let str = prompt("so'zni kiriting...")
+// let count = 0
+// for(let i = 0; i < str.length; i++){
+// 	let l = str[i]
+// 	if (l === "a" || l === "o" || l === "u" || l === "i" || l === "o'" || l === "e") {
+// 		count++
+// 	}
+// }
+// console.log(str.length - count);
+
+//8-masala
+// let num = +prompt("sonni kiriting")
+// let total = 0
+// for(i = 1; i < num; i++) {
+// 	if (num % i == 0) {
+// 		total += i
+// 	}
+// }
+// if (num === total) {
+// 	console.log("mukamml son");
+// }else{
+// 	console.log("mukammal son emas");
+// }
